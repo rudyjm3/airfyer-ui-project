83370c6810132d9d64e4b6f89f7af52e6e9dd4f7
Rudolph <rudyjm3@yahoo.com>
2024-04-24 22:16:17 -0400

    Added airfyer object, power on and off functions added.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,40 @@
+let airfyerUnit = {
+   power: false,
+   temperature: 180,
+   timer: 30,
+
+   powerOn: function() {
+      this.power = true;
+      let digitalDisplyContent = document.getElementsByClassName('digital-readout')[0].innerText = '350';
+      let tempSymbol = document.getElementsByClassName('temp-symbol')[0];
+      tempSymbol.classList.add('active-temp-symbol');
+      controlBtns.forEach(btn => {
+         btn.classList.add('active-icon');
+         btn.classList.add('active-icon-border');
+      });
+   },
+
+   powerOff: function() {  
+      this.power = false;
+      let digitalDisplyContent = document.getElementsByClassName('digital-readout')[0].innerText = '---';
+      let tempSymbol = document.getElementsByClassName('temp-symbol')[0];
+      tempSymbol.classList.remove('active-temp-symbol');
+      controlBtns.forEach(btn => {
+         btn.classList.remove('active-icon');
+         btn.classList.remove('active-icon-border');
+      });
+   },
+};
+
 const powerBtn = document.getElementsByClassName('power-btn')[0];
 const controlBtns = document.querySelectorAll('button');
 
 powerBtn.addEventListener('click', (ele) => {
-   controlBtns.forEach(btn => {
-      btn.classList.toggle('active-icon');
-      btn.classList.toggle('active-icon-border');
-   });
+   if(airfyerUnit.power === true){
+      airfyerUnit.powerOff();
+   } else {
+      airfyerUnit.powerOn();
+   }
 
 });
+
