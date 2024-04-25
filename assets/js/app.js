db1a8c3e1fdcea3cc13ff3230152bbb94b9b1f23
Rudolph <rudyjm3@yahoo.com>
2024-04-24 20:05:33 -0400

    Assigned click event to the power button only

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 const powerBtn = document.getElementsByClassName('power-btn')[0];
 const controlBtns = document.querySelectorAll('button');
 
-addEventListener('click', (ele) => {
+powerBtn.addEventListener('click', (ele) => {
    controlBtns.forEach(btn => {
       btn.classList.toggle('active-icon');
       btn.classList.toggle('active-icon-border');
