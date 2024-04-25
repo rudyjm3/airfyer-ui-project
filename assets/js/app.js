b216ddd6c165d2af0eacb528494d8a59ce11cb27
Rudolph <rudyjm3@yahoo.com>
2024-04-24 23:21:10 -0400

    Preprogrammed options annded to airfyer object. Function added to iterate through preprogrammed options when menu button clicked.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,45 @@
+const powerBtn = document.getElementsByClassName('power-btn')[0];
+const controlBtns = document.querySelectorAll('button');
+const menuIcons = document.querySelectorAll('.menu-icon');
+const menuBtn = document.getElementsByClassName('menu-btn')[0];
+
+
+// Airfyer Object
 let airfyerUnit = {
    power: false,
-   temperature: 180,
-   timer: 30,
-
+   temperature: 350,
+   timer: 15,
+   currentIndex: 0, // Track the index of the currently selected preprogrammed temperature
+   preprogrammedTemps: {
+      fries: {
+         temperature: 390,
+         timer: 20
+      },
+      lamb: {
+         temperature: 360,
+         timer: 15
+      },
+      shrimp: {
+         temperature: 320,
+         timer: 20
+      },
+      bake: {
+         temperature: 320,
+         timer: 40
+      },
+      chicken: {
+         temperature: 360,
+         timer: 25
+      },
+      steak: {
+         temperature: 320,
+         timer: 20
+      },
+      fish: {
+         temperature: 360,
+         timer: 20
+      }
+   },
    powerOn: function() {
       this.power = true;
       let digitalDisplyContent = document.getElementsByClassName('digital-readout')[0].innerText = '350';
@@ -24,17 +61,49 @@ let airfyerUnit = {
          btn.classList.remove('active-icon-border');
       });
    },
+   
+   iteratePreprogrammedTemps: function() {
+      // Get all keys (food types) from preprogramedTemps object
+      let foodTypes = Object.keys(this.preprogrammedTemps);
+      // console.log(foodTypes);
+      // Check if currentIndex is within bounds
+      if (this.currentIndex >= foodTypes.length) {
+         this.currentIndex = 0; // Reset index if out of bounds
+      }
+      
+      // Get the current food type based on currentIndex
+      let currentFoodType = foodTypes[this.currentIndex];
+      
+      // Retrieve the corresponding preprogrammed temperature
+      let selectedTemp = this.preprogrammedTemps[currentFoodType];
+      
+      // Increment currentIndex for next click
+      this.currentIndex++;
+      
+      // Log or use the selected temperature (for demonstration, let's log it)
+      console.log(`Selected ${currentFoodType}: Temperature ${selectedTemp.temperature}, Timer ${selectedTemp.timer}`);
+   }
 };
 
-const powerBtn = document.getElementsByClassName('power-btn')[0];
-const controlBtns = document.querySelectorAll('button');
+// Method to cycle through the preprogrammed temperatures
+let numberOfPreprogrammedTemps = Object.keys(airfyerUnit.preprogrammedTemps).length;
+// Log the number of preprogrammed temperatures
+console.log(numberOfPreprogrammedTemps);
 
+// Event Listeners
 powerBtn.addEventListener('click', (ele) => {
    if(airfyerUnit.power === true){
       airfyerUnit.powerOff();
    } else {
       airfyerUnit.powerOn();
    }
-
+});
+// Add event listener to menuButton to iterate through preprogrammedTemps
+menuBtn.addEventListener('click', function() {
+   if(airfyerUnit.power === true){
+      airfyerUnit.iteratePreprogrammedTemps();
+   } else {
+      alert('Turn on air fryer first before selecting preset');
+   };
 });
 
